5d03ebf23bcdfad65e29f03503135a68ac82bfd1
Aldo Garcia <yeimyhacker98@gmail.com>
2024-10-11 19:51:58 -0600

    cambio de nombre a PascalCase

--- client/fronted/EffiAutoTask/src/main.tsx
+++ client/fronted/EffiAutoTask/src/main.tsx
@@ -1,10 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
 import "./index.css";
 import { StrictMode } from "react";
 import Nav from "./components/Nav.tsx";
-import LandingPage from "./components/inicio.tsx";
+import LandingPage from "./components/Inicio.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
