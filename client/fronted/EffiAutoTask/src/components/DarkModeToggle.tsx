2a0d37d1b70a2cd8986655a0e8a88353139e63a5
aldogarcia-personal <yeimyhacker98@gmail.com>
2025-12-01 19:13:17 -0600

    Fix: Corregido tipo en FontAwesomeIcon y optimizado DarkModeToggle en la legibilidad y del estado ademas del ajustado el tipo de `prevMode` en el estado de darkMode

--- client/fronted/EffiAutoTask/src/components/DarkModeToggle.tsx
+++ client/fronted/EffiAutoTask/src/components/DarkModeToggle.tsx
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
 
 function DarkModeToggle() {
   const [darkMode, setDarkMode] = useState(() => {
-    const savedMode = localStorage.getItem("darkMode");
-    return savedMode ? JSON.parse(savedMode) : false;
-  });
+    const savedMode = localStorage.getItem("darkMode")
+    return savedMode ? JSON.parse(savedMode) : false
+  })
 
   useEffect(() => {
-    // Aplica la clase dark al body según el estado darkMode
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      document.body.classList.add("bg-gray-light", "dark:bg-gray-dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      document.body.classList.remove("bg-gray-light", "dark:bg-gray-dark");
-    }
-    // Guarda la preferencia en localStorage
-    localStorage.setItem("darkMode", JSON.stringify(darkMode));
-  }, [darkMode]);
+    const root = document.documentElement
+    const body = document.body
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+    if (darkMode) {
+      root.classList.add("dark")
+      body.classList.add("bg-gray-light", "dark:bg-gray-dark")
+    } else {
+      root.classList.remove("dark")
+      body.classList.remove("bg-gray-light", "dark:bg-gray-dark")
+    }
+
+    localStorage.setItem("darkMode", JSON.stringify(darkMode))
+  }, [darkMode])
+
+  const toggleDarkMode = () => setDarkMode((prevMode: any) => !prevMode)
 
   return (
     <div className="flex items-center">
@@ -44,7 +44,7 @@ function DarkModeToggle() {
         </div>
       </label>
     </div>
-  );
+  )
 }
 
-export default DarkModeToggle;
+export default DarkModeToggle
